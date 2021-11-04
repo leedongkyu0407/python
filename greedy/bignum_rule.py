60f5cdb2dce11c5dc5c8a5d03eea8979f8605044
leedongkyu0407 <use02012@gmail.com>
2021-11-04 16:53:27 +0900

    큰 수의 법칙

---
+++ greedy/bignum_rule.py
@@ -0,0 +1,34 @@
+def input_check(n, nums): #입력값에 오류 있을시 false return
+    if len(nums) != n:
+        return False
+    
+    return True
+
+def solution(n, m, k, nums):
+    
+    nums.sort() #정렬
+    max = nums[-1]
+    second_max = nums[-2]
+
+    result = 0
+
+    while True:
+        for i in range(k):
+            if m == 0: #m이 0이면 반복문 탈출
+                break
+            result += max
+            m -= 1
+        
+        if m == 0:
+            break
+        result += second_max
+        m -= 1
+    return result
+
+n, m, k = map(int, input().split())
+nums = list(map(int, input().split()))
+
+if input_check(n, nums):
+    print(solution(n, m, k, nums))
+else:
+    print("잘못된 입력입니다.")
