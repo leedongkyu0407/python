82e241ec925b219cece35db14d90cfdf69c66dde
leedongkyu0407 <use02012@gmail.com>
2021-11-08 09:56:55 +0900

    큰 수의 법칙

--- greedy/bignum_rule.py
+++ greedy/bignum_rule.py
@@ -7,8 +7,8 @@ def input_check(n, nums): #입력값에 오류 있을시 false return
 def solution(n, m, k, nums):
     
     nums.sort() #정렬
-    max = nums[-1]
-    second_max = nums[-2]
+    max = nums[-1] #가장 큰 수 
+    second_max = nums[-2] # 두번째로 큰 수
 
     result = 0
 
