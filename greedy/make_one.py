7331b1af192407767e6e365d734aa79ed5f75672
leedongkyu0407 <use02012@gmail.com>
2021-11-08 11:51:47 +0900

    1이 될 때까지

---
+++ greedy/make_one.py
@@ -0,0 +1,15 @@
+def solution(n, k):
+    count = 0
+
+    while n != 1:
+        if n % k == 0: #n이 k로 나누어떨어지면 나누기
+            n /= k
+        else:   #n이 k로 나누어떨어지지 않으면 1씩 빼기
+            n -= 1
+        
+        count += 1 #매 연산시 숫자 세기
+        
+    return count
+
+n, k = map(int, input().split())
+print(solution(n, k))
