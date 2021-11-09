fc073b7c8d0ec7a22b17b9d18860f3ceffac6eb6
leedongkyu0407 <use02012@gmail.com>
2021-11-09 11:41:15 +0900

    볼링공 고르기 풀이 2가지

--- greedy/choose_ball.py
+++ greedy/choose_ball.py
@@ -9,8 +9,27 @@ def solution(data):
 
     return case
 
+def solution_upgrade(n, m, data):
+    #1부터 10까지의 무게를 담을 수 있는 리스트
+    check = [0] * 11
+
+    #무게별 볼링공 개수 세기
+    for i in data:
+        check[i] += 1
+
+    case = 0
+    #1부터 m까지 각 무게에 대해 처리
+    for j in range(1, m+1):
+        #무게가 j인(A가 선택한 공의 무게와 동일한) 볼링공의 개수 제외
+        n -= check[j]
+        #B가 선택할 수 있는 경우의 수 곱하기
+        case += check[j] * n 
+
+    return case
+
 n, m = map(int, input().split())
 
 data = list(map(int, input().split()))
 
 print(solution(data))
+print(solution_upgrade(n, m, data))
