71de5607157e2431d5c29f10676e885074acea98
leedongkyu0407 <use02012@gmail.com>
2021-11-09 13:34:22 +0900

    설탕 배달

--- greedy/delivery_sugar.py
+++ greedy/delivery_sugar.py
@@ -2,13 +2,16 @@ def solution(n):
     result = 0
 
     while n >= 0:
+        #n이 5로 나누어 떨어지면 나눈 뒤 몫을 횟수에 더해서 반환
         if n % 5 == 0:
             result += (n//5)
             return result        
 
+        #n이 5로 나누어 떨어지지 않을 때는 횟수를 1씩 추가하며 3씩 뺴준다.
         n -= 3
         result += 1
 
+    #while문 종료 후에도 반환이 되지 않았다면 해가 존재하지 않음
     return -1
 
 n = int(input())
