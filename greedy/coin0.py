e0636c5bcc566024a3c4ef6336bfa3073174862c
leedongkyu0407 <use02012@gmail.com>
2021-11-10 17:52:14 +0900

    그리디 알고리즘

---
+++ greedy/coin0.py
@@ -0,0 +1,24 @@
+def solution(n, k, data):
+    #역순(내림차순)으로 정렬
+    data.sort(reverse = True)
+    result = 0 #코인 개수 
+
+    for i in range(n):
+        if k == 0:
+            break
+        #큰 것부터 확인하며 잔돈 계산
+        if data[i] <= k:
+            result += (k // data[i])
+            k %= data[i]
+
+    return result
+
+n, k = map(int, input().split())
+
+#코인 종류 저장 리스트
+data = [0] * n
+
+for i in range(n): #코인 입력값 저장
+    data[i] = int(input())
+
+print(solution(n, k, data))
