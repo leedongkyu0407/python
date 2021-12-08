509a408ad70c76e9eb287ade6198afda71ed0bc2
leedongkyu0407 <use02012@gmail.com>
2021-12-08 16:47:49 +0900

    시각https://www.notion.so/arrangementforanything/aa0fb276fac941408fed084ec3d2498b

---
+++ implementation/time.py
@@ -0,0 +1,16 @@
+import sys
+def solution(h):
+    count = 0
+    for i in range(h+1):
+        for j in range(60):
+            for k in range(60):
+                #매 시각 안에 '3'이 포함되어 있다면 카운트 증가
+                if '3' in str(i) + str(j) + str(k):
+                    count += 1
+
+
+    return count
+
+h = int(sys.stdin.readline())
+
+print(solution(h))
