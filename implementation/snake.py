aa1caf4b1cf516f790da26a7ec4674590fb118f2
leedongkyu0407 <use02012@gmail.com>
2022-03-28 21:24:58 +0900

    뱀https://www.notion.so/arrangementforanything/73814495d1694be4a0a15444f84d6e8e

--- implementation/snake.py
+++ implementation/snake.py
@@ -1,129 +1,75 @@
-n = int(input())
-k = int(input())
+#보드의 크기 사과 개수 입력 받기
+board_size = int(input())
+apple = int(input())
 
-apple_matrix = [[0]*n for _ in range(n)]
-snake_matrix = [[0]*n for _ in range(n)]
+#보드 생성
+board = [[0]*board_size for _ in range(board_size)]
 
-for i in range(k):
-    row, col = map(int, input().split())
-    apple_matrix[row-1][col-1] = 1
+#보드에 사과 위치 -1로 세팅
+for a in range(apple):
+    line, row = map(int, input().split())
+    board[line-1][row-1] = -1
 
-print(apple_matrix)
+#방향전환 횟수 입력
+direction_change = int(input())
+#dictionary 자료형을 이용하여 시간을 키로 방향 저장
+direction = {}
+for i in range(direction_change):
+    s,d = input().split()
+    direction[int(s)] = d
 
-change_dir = int(input())
-sec_dir = {}
-
-for i in range(change_dir):
-    s, d = input().split()
-    sec_dir[int(s)] = d
-
-snake_loc = [[0,0]]
-
-
-def solution(apple_matrix, snake_matrix, sec_dir):
-    sec = 1
-    direction = 0
+#메인 함수    
+def solution(board, direction):        
+    #초기 세팅
+    sec = 0
+    board[0][0] = 1
+    #현재 뱀이 차지하고 있는 좌표 리스트
+    snake_matrix = [[0,0]]
+    #현재 방향
+    direct_now = 0
+    #사과 유무 확인용 플래그
+    apple_flag = 0
+    #R, D, L, U
+    dx = [0, 1, 0, -1]
+    dy = [1, 0, -1, 0]
+    
+    #현재 x, y좌표
+    nx = 0
+    ny = 0
 
     while True:
-
         sec += 1
-
-        if sec in sec_dir.keys():
-            dir = sec_dir[sec]
-            direction = check_dir(direction, dir)
-
-        if direction < 0:
+        x = nx + dx[direct_now]
+        y = ny + dy[direct_now]
+        #뱀이 벽에 부딪히거나 자기 자신과 부딪힐 경우 종료
+        if (x > len(board)-1) or x < 0 or (y > len(board)-1) or y < 0 or board[x][y] == 1:
             return sec
+        
+        #사과가 있을 경우 플래그 설정 
+        if board[x][y] == -1:
+            apple_flag = 1
+        #머리 다음 칸으로 이동    
+        board[x][y] = 1
+        snake_matrix.append([x, y])
 
-        elif direction % 4 == 0:
-            check = dir_0(apple_matrix, snake_matrix, snake_loc)
+        #사과가 없으면 꼬리가 위치한 칸 비우기
+        if apple_flag != 1:
+            px, py = snake_matrix.pop(0)
+            board[px][py] = 0
+        else:
+            apple_flag = 0
 
-        elif direction % 4 == 1:
-            check = dir_1(apple_matrix, snake_matrix, snake_loc)
+        #방향 전환해야하는 시간일 경우
+        if sec in direction.keys():
+            if direction[sec] == 'D':
+                direct_now += 1
+            else:
+                direct_now -= 1
+            #dx, dy 리스트 활용을 위해 4로 나눈 나머지 활용    
+            direct_now %= 4
+            
+        nx = x
+        ny = y
+    return
 
-        elif direction % 4 == 2:
-            check = dir_2(apple_matrix, snake_matrix, snake_loc)
-
-        elif direction % 4 == 3:
-            check = dir_3(apple_matrix, snake_matrix, snake_loc)
-
-        if check == False:
-            return sec+1
-        print(len(snake_matrix))
-    return sec
-
-def check_dir(direction, d):
-    if d == 'D':
-        direction += 1
-    else:
-        direction -= 1
-    
-    return direction
-
-def dir_0(apple_matrix, snake_matrix, snake_loc):
-    r, c = snake_loc[-1]
-    if c+1 > len(snake_matrix)-1:
-        return False
-    
-    elif snake_matrix[r][c+1] == 1:
-        return False
-    
-    elif apple_matrix[r][c+1] != 1:
-        prev_r, prev_c = snake_loc.pop(0)
-        snake_matrix[prev_r][prev_c] = 0
-    
-    snake_matrix[r][c+1] = 1
-    snake_loc.append([r, c+1])
-    return 
-
-def dir_1(apple_matrix, snake_matrix, snake_loc):
-    r, c = snake_loc[-1]
-    if r+1 > len(snake_matrix)-1:
-        return False
-    
-    elif snake_matrix[r+1][c] == 1:
-        return False
-    
-    elif apple_matrix[r+1][c] != 1:
-        prev_r, prev_c = snake_loc.pop(0)
-        snake_matrix[prev_r][prev_c] = 0
-    
-    snake_matrix[r+1][c] = 1
-    snake_loc.append([r+1, c])
-    return 
-
-def dir_2(apple_matrix, snake_matrix, snake_loc):
-    r, c = snake_loc[-1]
-    if c-1 < 0:
-        return False
-    
-    elif snake_matrix[r][c-1] == 1:
-        return False
-    
-    elif apple_matrix[r][c-1] != 1:
-        prev_r, prev_c = snake_loc.pop(0)
-        snake_matrix[prev_r][prev_c] = 0
-    
-    snake_matrix[r][c-1] = 1
-    snake_loc.append([r, c-1])
-    return 
-
-def dir_3(apple_matrix, snake_matrix, snake_loc):
-    r, c = snake_loc[-1]
-    if r-1 < 0:
-        return False
-    
-    elif snake_matrix[r-1][c] == 1:
-        return False
-    
-    elif apple_matrix[r-1][c] != 1:
-        prev_r, prev_c = snake_loc.pop(0)
-        snake_matrix[prev_r][prev_c] = 0
-    
-    snake_matrix[r-1][c] = 1
-    snake_loc.append([r-1, c])
-    return 
-
-print(solution(apple_matrix, snake_matrix, sec_dir))
-
-#어딘가에서 인덱스 관련 버그 생긴 듯 (디버깅 ㄱㄱ)
+print(solution(board, direction))
